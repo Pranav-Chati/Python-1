a5ef4952ffad8f606ecd004565f62ad310142afd
Pranav Chati <31900598+Supa-HotFire@users.noreply.github.com>
2019-04-21 21:45:00 -0400

    Add files via upload

---
+++ FirstProgram/FirstProgram/FirstProgram.py
@@ -0,0 +1,14 @@
+#functions are in blue
+#use hastag to comment
+from datetime import date
+print(5)
+print (5.1)#prints the numer 5.1
+print(date.today()) #prints today's date
+
+#user inputing something
+myUserInput = input("Enter an integer: ")
+myUserInputAsInt = int(myUserInput) #this stores the string into an int file
+print(myUserInputAsInt + myUserInputAsInt)#the int file is added withit self
+#so this allows for the myuserinptasint to 5, making it print 10 etc.
+
+print("Hello World")
